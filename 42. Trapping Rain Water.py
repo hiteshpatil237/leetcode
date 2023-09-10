c764879dd9825660be58fa689600cca8b48c3863
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-09-09 21:23:26 -0400

    Submitted

---
+++ 42. Trapping Rain Water.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+        
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1  
+                rightMax = max(rightMax, height[r])      
+                res += rightMax - height[r]
+        
+        return res
